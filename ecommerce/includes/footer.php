aeb3ca4479cea58d7a6d389db18983388c139b58
KuMu3 <157594219+KuMu3@users.noreply.github.com>
2024-05-19 22:33:28 +0800

    Add files via upload

---
+++ ecommerce/includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2024 Brought to You By <a href="https://www.facebook.com/denmildz.da.77">Dextrous</a></strong>
+    </div>
+</footer>
